d33b3d966013decbcafb9bdae1f6f4d1ca9ee154
VIN07grinder <vihaankrao@gmail.com>
2025-04-09 17:16:36 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -3,16 +3,12 @@ const apexHost = location.host.startsWith("www.")
   : location.host;
 
 self.__uv$config = {
-  bare: [
-    `https://uv.${apexHost}/bare1/`,
-    `https://uv.${apexHost}/bare2/`,
-    `https://uv.${apexHost}/bare3/`,
-  ],
-  prefix: "/uv/~/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js"
+	prefix: "/service/",
+	encodeUrl: Ultraviolet.codec.xor.encode,
+	decodeUrl: Ultraviolet.codec.xor.decode,
+	handler: "/uv/uv.handler.js",
+	client: "/uv/uv.client.js",
+	bundle: "/uv/uv.bundle.js",
+	config: "/uv/uv.config.js",
+	sw: "/uv.sw.js",
 };
